11955de45f8a8bc366594bd3825220168cf905e5
Majid Ali <lahmajidali@gmail.com>
2024-04-21 18:41:33 +0500

    resolved cloudinary import bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,24 +7,29 @@ import { uploadOnCloudinary } from '../utils/cloudinary.js'
 const registerUser = asyncHandler(async (req, res) => {
   // get user credentials from client
   const { username, email, fullName, password } = req.body
+
   // validate request
   if (
     [username, email, fullName, password].some((field) => field.trim() === '')
   )
     throw new ApiError(400, 'All fields required')
+
   // check if user already exists via username & email
   const existingUser = User.findOne({
     $or: [{ username }, { email }],
   })
   if (existingUser)
     throw new ApiError(409, 'User with credentials already exists')
+
   // get images, avatar required
   const avatarLocalPath = req.files?.avatar[0]?.path
   const coverImageLocalPath = req.files?.localImage[0]?.path
+
   // upload to cloudinary, avatar required, after that get their cloudinary referance url
   if (!avatarLocalPath) throw new ApiError(400, 'Avatar file is required')
   const avatar = await uploadOnCloudinary(avatarLocalPath)
   const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
   // create user object, make db entry
   const user = await User.create({
     username: username.toLowerCase(),
@@ -34,13 +39,18 @@ const registerUser = asyncHandler(async (req, res) => {
     fullName,
     password,
   })
+
   // remove password and refreshToken from res
   const createduser = User.findById(user._id).select('-password -refreshToken')
+
   // confirm user creation
   if (!createduser)
     throw new ApiError(500, 'Something went wrong while regestring user')
+
   // return response
-  return res.status(201).json(new ApiResponse(200, 'User registered successfully', createduser))
+  return res
+    .status(201)
+    .json(new ApiResponse(200, 'User registered successfully', createduser))
 })
 
 export { registerUser }
